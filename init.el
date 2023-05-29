8ecd15688b24d90f83517f4a91bc8dc3a2e5f4ab
Yiyun Liu <liuyiyun@seas.upenn.edu>
2023-05-29 13:50:27 -0400

    Comment out the theme

--- init.el
+++ init.el
@@ -1,23 +1,23 @@
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'use-package)
 
-;;; nice dark theme
-(use-package modus-vivendi-theme
-  :straight t
-  :init
-  (load-theme 'modus-vivendi t))
+;;; uncomment the following to enable the dark theme
+;; (use-package modus-vivendi-theme
+;;   :straight t
+;;   :init
+;;   (load-theme 'modus-vivendi t))
 
 (use-package proof-general
   :straight t
